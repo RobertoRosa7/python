11d35581cd284c05446b6a2b77b77fadb869c458
Roberto Rosa <roberto.rosa7@gmail.com>
2019-09-22 20:17:13 -0300

    add and remove heapq

--- intro/list_queue.py
+++ intro/list_queue.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import heapq
 
 class ListQueue():
   
@@ -6,4 +7,17 @@ class ListQueue():
 
     '''
       Criando uma lista de prioridade em python
+      push - para inserção
+      pop - para remoção
     '''
+    self._queue = []
+    self._index = 0
+
+  def push(self, item, priority):
+    # add item
+    heapq.heappush(self._queue, (-priority, self._index, item))
+    self._index += 1
+  
+  def pop(self, item, priority):
+    # remove item
+    return heapq.heappop(self._queue)[-1]
