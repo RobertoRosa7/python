2c6059307588fc63bec63289822799c023fe9d2f
Roberto Rosa <roberto.rosa7@gmail.com>
2019-09-22 20:27:25 -0300

    list queue

--- intro/list_queue.py
+++ intro/list_queue.py
@@ -18,6 +18,22 @@ class ListQueue():
     heapq.heappush(self._queue, (-priority, self._index, item))
     self._index += 1
   
-  def pop(self, item, priority):
+  def pop(self):
     # remove item
     return heapq.heappop(self._queue)[-1]
+
+class Person():
+  def __init__(self, name):
+    self.name = name
+  
+  def __repr__(self):
+    return self.name
+
+priority = ListQueue()
+priority.push(Person('Kakashi'), 4)
+priority.push(Person('Maria'), 3)
+priority.push(Person('Camila'), 5)
+priority.push(Person('Gabriela'), 2)
+
+# return camila porque tem maior prioridade
+print(priority.pop()) 
