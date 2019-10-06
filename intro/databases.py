2fc8f111b0c0fc70c37b608b92cecb788297c35d
Roberto Rosa <roberto.rosa7@gmail.com>
2019-10-06 15:08:10 -0300

    chain map to maping

--- intro/databases.py
+++ intro/databases.py
@@ -4,19 +4,132 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 sequence = [10,-4,100,-3.55,0.004,-0.015,23,-12,-500,234,24,3421,-234,0.2,0.4,0.55]
 mix = ['Brasil', 2, 30.5, True, 'Estados Unidos', False, 5.2, 'Brasil']
 people = {'nome':'Roberto','idade': 32,'cidade': 'Osasco','altura': 1.75,'peso': 65.3}
-names = ['Roberto','Maria','Camila','Sandra','Rose','Roberto','Maria','Elizabete','Monica','Pedro','Thales','Leonardo','Rose','Maria','Fernanda','Bruna','Jose','Monica','Beatriz','Gabriela','Juliana','Sonia','Fernanda','Rose','Roberto','Maria','Sandra','Gustavo']
-products = {'notebook':5999, 'smartphone':1999,'desktop':2000,'setphone':59,'phone':199,'monitor':215,'mouse':29,'pendrive':189,'hard disk':459,'usb cable':129}
+names = [
+    'Roberto',
+    'Maria',
+    'Camila',
+    'Sandra',
+    'Rose',
+    'Roberto',
+    'Maria',
+    'Elizabete',
+    'Monica',
+    'Pedro',
+    'Thales',
+    'Leonardo',
+    'Rose',
+    'Maria',
+    'Fernanda',
+    'Bruna',
+    'Jose',
+    'Monica',
+    'Beatriz',
+    'Gabriela',
+    'Juliana',
+    'Sonia',
+    'Fernanda',
+    'Rose',
+    'Roberto',
+    'Maria',
+    'Sandra',
+    'Gustavo'
+]
+
+products = {
+    'notebook':5999, 
+    'smartphone':1999,
+    'desktop':2000,
+    'setphone':59,
+    'phone':199,
+    'monitor':215,
+    'mouse':29,
+    'pendrive':189,
+    'hard disk':459,
+    'usb cable':129
+}
 database = [
-    {'name':'Roberto','age':32,'city':'Osasco','height':1.75,'weight':65.3},
-    {'name':'Sandra','age':22,'city':'São Paulo','height':1.54,'weight':45.3},
-    {'name':'Maria','age':24,'city':'São Paulo','height':1.60,'weight':55.3},
-    {'name':'Gabriela','age':31,'city':'Osasco','height':1.48,'weight':50.5},
-    {'name':'George','age':18,'city':'Barueri','height':1.89,'weight':95.3},
-    {'name':'Thales','age':35,'city':'Itaquera','height':1.85,'weight':102.9},
-    {'name':'Jessica','age':25,'city':'Bonfiglioli','height':1.58,'weight':73.2},
-    {'name':'Bruna','age':42,'city':'São Paulo','height':1.72,'weight':89.9},
-    {'name':'Pedro','age':28,'city':'Osasco','height':1.65,'weight':65.9},
-    {'name':'Yan','age':22,'city':'Barra funda','height':1.55,'weight':55.0},
-    {'name':'Rodrigo','age':35,'city':'Santa cecília','height':1.80,'weight':85.3},
-    {'name':'Patricia','age':18,'city':'Osasco','height':1.69,'weight':57.9},
+    {
+        'name':'Roberto',
+        'age':32,
+        'city':'Osasco',
+        'height':1.75,
+        'weight':65.3
+    },
+    {
+        'name':'Sandra',
+        'age':22,
+        'city':'São Paulo',
+        'height':1.54,
+        'weight':45.3
+    },
+    {
+        'name':'Maria',
+        'age':24,
+        'city':'São Paulo',
+        'height':1.60,
+        'weight':55.3
+    },
+    {
+        'name':'Gabriela',
+        'age':31,
+        'city':'Osasco',
+        'height':1.48,
+        'weight':50.5
+    },
+    {
+        'name':'George',
+        'age':18,
+        'city':'Barueri',
+        'height':1.89,
+        'weight':95.3
+    },
+    {
+        'name':'Thales',
+        'age':35,
+        'city':'Itaquera',
+        'height':1.85,
+        'weight':102.9
+    },
+    {
+        'name':'Jessica',
+        'age':25,
+        'city':'Bonfiglioli',
+        'height':1.58,
+        'weight':73.2
+    },
+    {
+        'name':'Bruna',
+        'age':42,
+        'city':'São Paulo',
+        'height':1.72,
+        'weight':89.9
+    },
+    {
+        'name':'Pedro',
+        'age':28,
+        'city':'Osasco',
+        'height':1.65,
+        'weight':65.9
+    },
+    {
+        'name':'Yan',
+        'age':22,
+        'city':'Barra funda',
+        'height':1.55,
+        'weight':55.0
+    },
+    {
+        'name':'Rodrigo',
+        'age':35,
+        'city':'Santa cecília',
+        'height':1.80,
+        'weight':85.3
+    },
+    {
+        'name':'Patricia',
+        'age':18,
+        'city':'Osasco',
+        'height':1.69,
+        'weight':57.9
+    }
 ]
