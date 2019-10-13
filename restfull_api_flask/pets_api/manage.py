8113cee8a5cfb06967ea559153e1e49aba6fa4dc
Roberto Rosa <roberto.rosa7@gmail.com>
2019-10-13 18:11:14 -0300

    create manage file to run app

---
+++ restfull_api_flask/pets_api/manage.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# set path
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from flask_script import Manager, Server
+from application import app
+
+app = app()
+manager = Manager(app)
+
+# Turn on debug
+manager.add_command('runserver', Server(
+  use_debugger=True,
+  use_reloader=True,
+  host= os.getenv('IP', '0.0.0.0'),
+  port=int(os.getenv('PORT', 5000)))
+)
+
+if __name__ == '__main__':
+  manager.run()
