07a99045b7a74753c41e54be011b05e734a8c407
Roberto Rosa <roberto.rosa7@gmail.com>
2019-10-13 19:50:31 -0300

    dokcer image install

--- restfull_api_flask/pets_api/manage.py
+++ restfull_api_flask/pets_api/manage.py
@@ -4,16 +4,16 @@ import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from flask_script import Manager, Server
-from application import app
+from application import create_app
 
-app = app()
+app = create_app()
 manager = Manager(app)
 
 # Turn on debug
 manager.add_command('runserver', Server(
   use_debugger=True,
   use_reloader=True,
-  host= os.getenv('IP', '0.0.0.0'),
+  host=os.getenv('IP', '0.0.0.0'),
   port=int(os.getenv('PORT', 5000)))
 )
 
