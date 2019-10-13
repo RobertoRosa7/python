430d1242576ea1c8b6f5595f55499fbf91c21854
Roberto Rosa <roberto.rosa7@gmail.com>
2019-10-13 18:20:23 -0300

    create home e view

---
+++ restfull_api_flask/pets_api/application.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+from flask_mongoengine import MongoEngine
+
+db = MongoEngine()
+
+def create_app(**config_overrides):
+  app = Flask(__name__)
+
+  # loading config
+  app.config.from_pyfile('settings.py')
+
+  # apply overrides for test
+  app.config.update(config_overrides)
+
+  # set db
+  db.init_app(app)
+
+  # import blueprints
+  from home.views import home_app
+
+  # register blueprints
+  app.register_blueprint(home_app)
+
+  return app
