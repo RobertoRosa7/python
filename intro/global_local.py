cb6ddec795a70d7dac50c4f3a2b9f6d5e7875806
Roberto <roberto.rosa7@gmail.com>
2020-06-09 18:41:00 -0300

    variable global and local

---
+++ intro/global_local.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# GLABAL AND LOCAL SCOPE
+num = 43 # Global variable
+
+def spam():
+  num = 'Hello'
+  print(num)
+
+spam()
+print(num)
+# def eggs():
+  # return num = 43 # não funciona assim
+  # num = 43 # Local variable
+  # spam()
+  # print(num)
+  # return num
+
+# def spam():
+#   egg = 99
+#   rams = 0
