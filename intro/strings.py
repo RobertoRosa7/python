e338079e1f905af3a83894c3b12e6581e0fcf4bb
Roberto <roberto.rosa7@gmail.com>
2020-06-13 12:11:21 -0300

    formatando código para nova versão de python pycharm

--- intro/strings.py
+++ intro/strings.py
@@ -2,89 +2,79 @@
 from fnmatch import fnmatch, fnmatchcase
 import re
 
-def findMatch():
-  '''
+
+def find_match():
+  """
     Muito utilizado para URL's isso indica uma exata correspondência de caracteres
     o método match só verifica o início de uma string, e (r) é usado para strings simples
     que não há necessidade de usar barra invertida
-  '''
-  # print(fnmatch('arquivo.txt', '*.txt')) # return true
-  # print(fnmatch('file.py', '*.txt')) # return false
-  # print(fnmatch('arquivo0001.txt', 'arquivo[0-9]*')) # return true
-  # print(fnmatch('arquivo002.png', '*.txt')) # return false
-  # print(fnmatchcase('arq.txt', '*.TXT')) # return false
 
-  # data = '06/11/2019'
-  # url = 'https://'
-  defaultPattern = re.compile(r'\d+/\d+/\d+')
-  # response = True if re.match(r'\d+/\d+/\d+', url) else False
-  # response = True if defaultPattern.match(url) else False
-  # print(data, response)
+    print(fnmatch('arquivo.txt', '*.txt')) return true
+    print(fnmatch('file.py', '*.txt')) return false
+    print(fnmatch('arquivo0001.txt', 'arquivo[0-9]*')) return true
+    print(fnmatch('arquivo002.png', '*.txt')) return false
+    print(fnmatchcase('arq.txt', '*.TXT')) return false
+  """
+  url = 'https://'
+  default_pattern = re.compile(r'\d+/\d+/\d+') # default pattern
+  match_data = re.compile(r'\d+/\d+/d+\d+')  # data = '06/11/2019'
+  match_url = default_pattern.match(url)  # url = 'https://'
 
-  # find all occurrence
+  response = True if re.match(r'\d+/\d+/\d+', url) else False
+  response = True if default_pattern.match(url) else False
+
   texto = 'blablablabla, 11/12/2019, blablablablablabla, 01/01/2020'
-  print(defaultPattern.findall(texto))
+  print(default_pattern.findall(texto))  # find all occurrence
+
 
 def strings():
-    '''
-        Manipulação de strings
-    '''
-    # substituir uma palavra da string
-    texto = 'eu amo estudar javascript, mas no meu tempo livre estudo python'
-    print(texto.replace('javascript', 'php'))
+  """
+      Manipulação de strings
+      acessar cada caracter
+      nome = 'roberto'
+      substituir algum caracter por outro
+      nome = 'roberto'
+      lista = list(nome) necessário para substituíção
+      lista[0] = 't'
+      nome = ''.join(lista)
+      para saber o final de uma string | .txt | .png | .pdf | .zip etc...
+      file = 'arquivo.txt'
+      print(file.endswith('.txt')) return boolean
+      print(file.startswith('arq')) return boolean
+  """
 
-    # substituir um padrão dentro da string
-    texto02 = 'eu nasci na data de 26/02/1987 e minha irmã nasceu em 10/12/1980'
-    print(re.sub(r'(\d+)/(\d+)/(\d+)', r'\3-\1-\2', texto02))
-    print(re.sub(r'(\d+)/(\d+)/(\d+)', r'\3-\1-\2', texto02, flags=re.IGNORECASE)) # ignorar case sensitive
+  texto = 'eu amo estudar javascript, mas no meu tempo livre estudo python'
+  print(texto.replace('javascript', 'php'))  # substituir uma palavra da string
 
-    texto03 = 'eu nasci em 1987-02-26'
-    print(re.sub(r'(\d+)-(\d+)-(\d+)', r'\3/\2/\1', texto03))
+  texto02 = 'eu nasci na data de 26/02/1987 e minha irmã nasceu em 10/12/1980'
+  print(re.sub(r'(\d+)/(\d+)/(\d+)', r'\3-\1-\2', texto02))  # substituir um padrão dentro da string
+  print(re.sub(r'(\d+)/(\d+)/(\d+)', r'\3-\1-\2', texto02, flags=re.IGNORECASE))  # ignorar case sensitive
 
-    # remover espaço em branco
-    texto04 = '      python    '
-    print(texto04.strip())
+  texto03 = 'eu nasci em 1987-02-26'
+  print(re.sub(r'(\d+)-(\d+)-(\d+)', r'\3/\2/\1', texto03))
 
-    # remover caracteres repetidos no inicio
-    texto05 = '----------javascript---------'
-    print(texto05.strip('-'))
+  texto04 = '      python    '
+  print(texto04.strip())  # remover espaço em branco
 
-    # remover apenas do lado esquerdo
-    texto06 = '------------php'
-    print(texto06.lstrip('-'))
+  texto05 = '----------javascript---------'
+  print(texto05.strip('-'))  # remover caracteres repetidos no inicio
 
-    # remover apenas do lado direito
-    texto07 = 'mysql----------'
-    print(texto07.rstrip('-'))
-    # acessar cada caracter
-    # nome = 'roberto'
+  texto06 = '------------php'
+  print(texto06.lstrip('-'))  # remover apenas do lado esquerdo
 
-    # substituir algum caracter por outro
-    # nome = 'roberto'
-    # lista = list(nome) # necessário para substituíção 
-    # lista[0] = 't'
-    # nome = ''.join(lista)
+  texto07 = 'mysql----------'
+  print(texto07.rstrip('-'))  # remover apenas do lado direito
 
-    # para saber o final de uma string | .txt | .png | .pdf | .zip etc...
-    # file = 'arquivo.txt'
-    # print(file.endswith('.txt')) # return boolean
-    # print(file.startswith('arq')) # return boolean
 
 strings()
-def stringImutaveis():
-  '''
+
+
+def string_imutaveis():
+  """
     Strings in python são imutáveis por não poder receber atribuição de novos valores em suas
-    
     string = python
     string[0] = 'a' error não é possível realizar esta instrução
-  '''
-
-  # não é possível
-  # name = 'python'
-  # name[0] = 'j'
-
-  # jeito correto de alterar um caracter na sua posição
+  """
   name = 'python'
   name = 'J' + name[1:]
-
   print(name)
