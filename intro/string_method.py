0dc3df0fb0378fb2086579812401977f9ff368cc
Roberto <roberto.rosa7@gmail.com>
2020-06-22 09:23:52 -0300

    working with manipulation texts

---
+++ intro/string_method.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+STRING METHOD
+"""
+
+text = 'This is a text'
+
+text.lower()  # convert to text lowercase
+text.upper()  # convert to text uppercase
+text.islower()  # return boolean
+text.isupper()  # return boolean
+text.upper().isupper()  # statement valid
+text.isalpha()  # letters only
+text.isspace()  # whitespace only
+text.istitle()  # titlecase only
+text.isalnum()  # letters and numbers only
+text.isdecimal()  # numbers only
+
