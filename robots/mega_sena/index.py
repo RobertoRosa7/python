83831a6e91693a7d50560b9ca61be1e6353bd37f
RobertoRosa7 <roberto.rosa7@gmail.com>
2020-11-03 14:55:41 -0300

    create method to convert json to csv

--- robots/mega_sena/index.py
+++ robots/mega_sena/index.py
@@ -3,6 +3,9 @@ import numpy as np
 import pandas as pd
 import random
 import math
+import os
+import json
+import csv
 
 payload = {}
 
@@ -19,6 +22,25 @@ def generateNumber(min=1, max=60, array=[], index=0):
     return generateNumber(min, max, array) if rand in array else rand
 
 
-payload['ticker'] = otherWayToGenerateArray(6)
-
-print(payload)
+def converte_json_to_csv():
+    # dcsv = pd.read_csv(os.path.join(os.getcwd(), 'databases/mega.csv'))
+    # djson = pd.read_json(os.path.join(os.getcwd(), 'databases/mega.json'))
+    # dataCsv = dataJson.to_csv(os.path.join(os.getcwd(), 'databases/mega.csv'), index=None)
+    
+    with open(os.path.join(os.getcwd(), 'databases/mega.json'), 'r') as f:
+        to_python = json.loads(f.read())
+        with open(os.path.join(os.getcwd(), 'databases/mega.csv'), 'w') as fcsv:
+            fs = csv.writer(fcsv)
+            fs.writerow(['id', 'ticket', 'date', 'concurso', 'created_at'])
+            for i, data in enumerate(to_python['mega']):
+                fs.writerow(
+                    [
+                        i,
+                        data['content'],
+                        data['date'],
+                        data['concurso'],
+                        data['create_at']
+                    ]
+                )
+        fcsv.close()
+    f.close()
