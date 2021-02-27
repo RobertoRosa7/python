bdc5ed1a77d473046c708b2f1361889409a3e2a1
Roberto Rosa <roberto.rosa7@gmail.com>
2021-02-27 03:56:27 -0300

    pandas para filter de categorias e limpar strings

--- api_server/routes/dashboard.py
+++ api_server/routes/dashboard.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
-import os, sys, json
+import os, sys, json, re
 import asyncio
 import pymongo
 import datetime
 import time
+import pandas as pd
+import numpy as np
+
+
 sys.path.append(os.path.abspath(os.getcwd()))
 
 from datetime import date, datetime as dt
@@ -36,32 +40,55 @@ def fetch_registers():
     days = request.args.get('days', default=7, type=int)
     todos = request.args.get('todos', default=None, type=str)
     data = {'results': [] }
+    evolucao = {'days': [], 'values': []}
     rangeDates = []
     filtered = {}
 
     for i in range(0, days):
       rangeDates.append((datetime.date.today() - datetime.timedelta(i)).isoformat())
-    
+
     if todos == None:
       filtered['created_at'] = {
-        '$lte': int(time.mktime(datetime.datetime.strptime(rangeDates[0], "%Y-%m-%d").timetuple())*1e3), 
-        '$gte': int(time.mktime(datetime.datetime.strptime(rangeDates[-1], "%Y-%m-%d").timetuple())*1e3)
+        '$lte': float(time.mktime(datetime.datetime.strptime(rangeDates[0], "%Y-%m-%d").timetuple())), 
+        '$gte': float(time.mktime(datetime.datetime.strptime(rangeDates[-1], "%Y-%m-%d").timetuple()))
       }
-
+    
     result = db.collection_registers.find(filtered).sort('created_at', pymongo.DESCENDING)
     
+    # print(int(time.mktime(datetime.datetime.strptime(rangeDates[0], "%Y-%m-%d").timetuple())/1e3))
     dumps_result = dumps(result)
+    
+    df = pd.read_json(dumps_result)
+    categories = pd.DataFrame(df['category']).drop_duplicates().values
+
+    for cat in categories:
+      category_format = cat[0].lower().replace(' ', '_').replace('&', 'e').replace('á','a').replace('ã','a').replace('ç','c').replace('õ','o')
+      print(category_format)
+
+    # print(len(np.array(categories)))
+
     str_to_json = json.loads(dumps_result)
     data['consolidado'] = calcular_consolidado(str_to_json)
+    
 
-    # print('start: {1} end: {0}'.format(rangeDates[0], rangeDates[-1]))
-    # str_to_timestamp = time.mktime(datetime.datetime.strptime(rangeDates[0], "%Y-%m%d").timetuple())
-    # print(str_to_timestamp)
     for i in range(len(str_to_json)):
+      # temp_id = ObjectId(str_to_json[i]['_id']['$oid'])
+      # del str_to_json[i]['_id']
+
       # conveter timestamp javascript to string
       # date = datetime.datetime.fromtimestamp(str_to_json[i]['created_at'] / 1e3).strftime('%Y-%m-%d')
+      
+      # date = int(time.mktime(datetime.datetime.strptime(
+        # str_to_json[i]['created_at'], "%Y-%m-%d").timetuple()))
+      # db.collection_registers.update_one({"_id": temp_id}, {"$set": {'created_at': date }})
+
+      # evolucao['days'].append(str_to_json[i]['created_at'])
+      # evolucao['values'].append(str_to_json[i]['value'] / 100)
       data['results'].append(str_to_json[i])
 
+    
+    # data['evolucao'] = evolucao
+    data['total'] = len(str_to_json)
     response = jsonify({'status':200, 'msg': 'total de registros: ' + str(len(str_to_json)), 'data': data})
     response.status_code = 200
     return response
@@ -85,24 +112,15 @@ def new_register():
 
 @dashboard.route("/calc_consolidado", methods=["GET"])
 def calc_consolidado():
-  payload = {
-      "total_credit": 0,
-      "total_debit": 0,
-      "total_consolidado": 0
-    }
   try:
-    result = db.collection_registers.find()
-    response = dumps(result)
-    data = json.loads(response)
-
-    for i in range(len(data)):
-        if data[i]['type'] == 'incoming':
-            payload['total_credit'] += int(data[i]['value'])
-        elif data[i]['type'] == 'outcoming':
-            payload['total_debit'] += int(data[i]['value'])
-
-    payload['total_consolidado'] += (payload['total_credit'] - payload['total_debit'])
-    return jsonify(payload)
+    result = db.collection_registers.find().sort('created_at', pymongo.DESCENDING)
+    dumps_result = dumps(result)
+    str_to_json = json.loads(dumps_result)
+    consolidado = calcular_consolidado(str_to_json)
+    
+    response = jsonify(consolidado)
+    response.status_code = 200
+    return response
   except Exception as e:
     return not_found(e)
 
@@ -171,6 +189,30 @@ def get_code():
     return str(json.dumps(data)), 200
   except Exception as e:
     return not_found(e)
+
+
+def get_first_date(invs, salfer=False):
+  dt_lower = time.time()
+  for inv in invs:
+    dt_inv = inv['created_at']
+    if dt_inv < dt_lower:
+        dt_lower = dt_inv
+  return dt_lower
+
+
+def get_last_date(invs):
+  last_date = None
+  for inv in invs:
+    current_date = inv.get('created_at', None)
+    if not current_date or current_date > time.time():
+      last_date = time.time()
+      break
+    else:
+      if last_date == None:
+        last_date = current_date
+      elif current_date > last_date:
+        last_date = current_date
+  return last_date
 
 
 @dashboard.errorhandler(404)
