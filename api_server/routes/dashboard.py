e7e8bc8df1f6e3d529f3752f6a2eb63228d1807a
Roberto Rosa <roberto.rosa7@gmail.com>
2021-03-02 12:33:25 -0300

    usando api prod mongo

--- api_server/routes/dashboard.py
+++ api_server/routes/dashboard.py
@@ -6,7 +6,7 @@ import datetime
 import time
 import pandas as pd
 import numpy as np
-
+import pprint
 
 sys.path.append(os.path.abspath(os.getcwd()))
 
@@ -49,47 +49,75 @@ def fetch_registers():
 
     if todos == None:
       filtered['created_at'] = {
-        '$lte': float(time.mktime(datetime.datetime.strptime(rangeDates[0], "%Y-%m-%d").timetuple())), 
+        '$lte': time.time(), 
         '$gte': float(time.mktime(datetime.datetime.strptime(rangeDates[-1], "%Y-%m-%d").timetuple()))
       }
     
-    result = db.collection_registers.find(filtered).sort('created_at', pymongo.DESCENDING)
-    
-    # print(int(time.mktime(datetime.datetime.strptime(rangeDates[0], "%Y-%m-%d").timetuple())/1e3))
-    dumps_result = dumps(result)
-    
-    df = pd.read_json(dumps_result)
-    categories = pd.DataFrame(df['category']).drop_duplicates().values
+    result = list(db.collection_registers.find(filtered).sort('created_at', pymongo.DESCENDING))
 
-    for cat in categories:
-      category_format = cat[0].lower().replace(' ', '_').replace('&', 'e').replace('á','a').replace('ã','a').replace('ç','c').replace('õ','o')
-      print(category_format)
-
-    # print(len(np.array(categories)))
-
-    str_to_json = json.loads(dumps_result)
-    data['consolidado'] = calcular_consolidado(str_to_json)
-    
-
-    for i in range(len(str_to_json)):
-      # temp_id = ObjectId(str_to_json[i]['_id']['$oid'])
-      # del str_to_json[i]['_id']
-
-      # conveter timestamp javascript to string
-      # date = datetime.datetime.fromtimestamp(str_to_json[i]['created_at'] / 1e3).strftime('%Y-%m-%d')
+    if len(result) > 0:
+      # print(int(time.mktime(datetime.datetime.strptime(rangeDates[0], "%Y-%m-%d").timetuple())/1e3))
+      dumps_result = dumps(result)
       
-      # date = int(time.mktime(datetime.datetime.strptime(
-        # str_to_json[i]['created_at'], "%Y-%m-%d").timetuple()))
-      # db.collection_registers.update_one({"_id": temp_id}, {"$set": {'created_at': date }})
+      df = pd.read_json(dumps_result)
+      get_to_create_list = pd.DataFrame(df['category']).drop_duplicates().values
+      get_to_build_list = pd.DataFrame(df[['category', 'value', 'created_at', 'type']]).values
 
-      # evolucao['days'].append(str_to_json[i]['created_at'])
-      # evolucao['values'].append(str_to_json[i]['value'] / 100)
-      data['results'].append(str_to_json[i])
+      # print(get_to_build_list)
+      build_categories = {}
+      build_list = []
+      values = {'values': []}
+      dates = {'dates': []}
+      total = 0
+      media = 0
 
+      for cat in get_to_create_list:
+        category_format = cat[0].lower().replace(' ', '_').replace('&', 'e').replace('á','a').replace('ã','a').replace('ç','c').replace('õ','o')
+
+      for i in get_to_build_list:
+        cat_name = i[0].lower().replace(' ', '_').replace('&', 'e').replace('á','a').replace('ã','a').replace('ç','c').replace('õ','o')
+
+        if cat_name == 'alimentacao':
+          values['values'].append(i[1])
+          dates['dates'].append(i[2])
+          build_categories[cat_name] = dates
+          build_categories[cat_name] = values
+          total += i[1]
+          # build_categories[cat_name] = values
+          # values.append(i[1])
+          # dates.append(i[2])
+          # build_categories['value'] = values
+          # build_categories['created_at'] = dates
+          # build_categories[cat_name] = i[0]
+          # build_categories['type'] = i[3]
+          
+      # media = total / len(values)
+      # build_categories = {
+      #   'alimentacao': {
+      #     'values': values,
+      #     'dates': dates,
+      #     'total': total,
+      #     'media': media
+      #   }
+      # }
+
+      # build_list.append(build_categories)
+
+      # pp = pprint.PrettyPrinter(indent=2)
+      # pp.pprint(build_list)
+
+      str_to_json = json.loads(dumps_result)
+      data['consolidado'] = calcular_consolidado(str_to_json)
+      
+
+      for i in range(len(str_to_json)):
+        data['results'].append(str_to_json[i])
+
+      
+      # data['evolucao'] = evolucao
+      data['total'] = len(str_to_json)
     
-    # data['evolucao'] = evolucao
-    data['total'] = len(str_to_json)
-    response = jsonify({'status':200, 'msg': 'total de registros: ' + str(len(str_to_json)), 'data': data})
+    response = jsonify({'data': data})
     response.status_code = 200
     return response
   except Exception as e:
