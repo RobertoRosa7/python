f6b8a6ee0580ad81595952e339d15cc20d0c136f
Roberto Rosa <roberto.rosa7@gmail.com>
2022-07-28 10:23:50 -0300

    python - calc sum of two lists

--- intro/arquivos.py
+++ intro/arquivos.py
@@ -17,6 +17,8 @@
     readline() - lê apenas uma linha
     readelines() - lê o arquivo e gera nova lista (array)
 """
+import random
+
 
 def first():
     # Nova instancia para conter todo conteúdo do arquivo
@@ -46,11 +48,13 @@ def first():
     file.close()
     file2.close()
 
+
 def second():
     # abrir arquivo com alias
     with open('arquivo_este2', 'r') as f:
         print(f.read())
-    f.close() # importante fechar sempre que abrir um arquivo
+    f.close()  # importante fechar sempre que abrir um arquivo
+
 
 def third():
     # escrendo arquivo
@@ -58,10 +62,34 @@ def third():
         f.write('Este arquivo foi escrito pelo python:\n')
     f.close()
 
+
 # 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
 def fib(n):
     if n == 1 or n == 2:
         return 1
     return fib(n - 1) + fib(n - 2)
 
+
 # print(fib(7)) # return 13
+
+def calc_sum(list_a, list_b):
+    return [(v1 + v2) for v1, v2 in zip(list_a, list_b)]
+
+
+def calc_sum2(list_a, list_b):
+    return list(map(lambda v1, v2: v1 + v2, list_a, list_b))
+
+
+def calc_sum3(list_a, list_b):
+    return list(map(sum, zip(list_a, list_b)))
+
+
+def generate_range(size):
+    lists = []
+    for i in range(size):
+        lists.append(random.randint(0, 9))
+    return lists
+
+
+num = calc_sum3(generate_range(10), generate_range(10))
+print(num)
--- utils/formats.py
+++ utils/formats.py
@@ -6,136 +6,136 @@ from flask import make_response
 
 
 class Formats:
-    """Formats
+  """Formats
 
-    Utils
-    -----
-    Para uso de recursos que podem ser utils
+  Utils
+  -----
+  Para uso de recursos que podem ser utils
+  """
+
+  def debug(self, **Kwargs):
+    """Debug: string"""
+    for key, value in Kwargs.items():
+      print("key %s, value %s" % (key, value))
+
+  def path_database(self, filename):
     """
+    Return : String
+    Notes
+    -----
+      Retorna uma string com o path completo para o diretório databases
 
-    def debug(self, **Kwargs):
-        """Debug: string"""
-        for key, value in Kwargs.items():
-            print("key %s, value %s" % (key, value))
+    """
+    try:
+      return os.path.join(os.path.dirname(__file__), "../databases/" + filename)
+      # return os.path.abspath(
+      #     os.path.join(
+      #         os.path.dirname(os.getcwd()), "python/databases/" + filename
+      #     )
+      # )
+    except FileNotFoundError:
+      return "Arquivo ou Diretório não encontrado"
 
-    def path_database(self, filename):
-        """
-        Return : String
-        Notes
-        -----
-          Retorna uma string com o path completo para o diretório databases
+  def convert_csv_to_json(self, file_csv):
+    """
+    Parameter
+    ---------
+    file_csv : String, default=None
 
-        """
-        try:
-            return os.path.join(os.path.dirname(__file__), "../databases/" + filename)
-            # return os.path.abspath(
-            #     os.path.join(
-            #         os.path.dirname(os.getcwd()), "python/databases/" + filename
-            #     )
-            # )
-        except FileNotFoundError:
-            return "Arquivo ou Diretório não encontrado"
+    Notes
+    -----
+    Converter um arquivo CSV para formato JSON, para uso do JavaScript em retorno API
 
-    def convert_csv_to_json(self, file_csv):
-        """
-        Parameter
-        ---------
-        file_csv : String, default=None
+    """
+    json_file = {}
 
-        Notes
-        -----
-        Converter um arquivo CSV para formato JSON, para uso do JavaScript em retorno API
+    with open(file_csv) as read_file:
+      reader = csv.DictReader(read_file)
+      for i, row in enumerate(reader):
+        json_file[i] = row
+    read_file.close()
+    return json_file
 
-        """
-        json_file = {}
+  def json_response(self, obj, status_code=200, cls=None):
+    response = make_response(json.dumps(obj, cls=cls), status_code)
+    response.content_type = "application/json"
+    return response
 
-        with open(file_csv) as read_file:
-            reader = csv.DictReader(read_file)
-            for i, row in enumerate(reader):
-                json_file[i] = row
-        read_file.close()
-        return json_file
+  def check_list_csv_repeated(self, filepath):
+    if not filepath:
+      return []
+    list_repeated = list()
+    with open(self.path_database(filepath)) as rf:
+      temp = list()
+      for row in rf:
+        find = self.find_ticket(row)
+        for i in find:
+          temp.append(i)
 
-    def json_response(self, obj, status_code=200, cls=None):
-        response = make_response(json.dumps(obj, cls=cls), status_code)
-        response.content_type = "application/json"
-        return response
+      list_repeated = pd.Series(temp)[pd.Series(temp).duplicated()].values
+    rf.close()
+    return list_repeated
 
-    def check_list_csv_repeated(self, filepath):
-        if not filepath:
-            return []
-        list_repeated = list()
-        with open(self.path_database(filepath)) as rf:
-            temp = list()
-            for row in rf:
-                find = self.find_ticket(row)
-                for i in find:
-                    temp.append(i)
+  def find_ticket(self, text):
+    return re.compile(
+      r"(\d{2}|\d{1}),\s(\d{2}|\d{1}),\s(\d{2}|\d{1}),\s(\d{2}|\d{1}),\s(\d{2}|\d{1}),\s(\d{2}|\d{1})"
+    ).findall(text)
 
-            list_repeated = pd.Series(temp)[pd.Series(temp).duplicated()].values
-        rf.close()
-        return list_repeated
+  def remove_ticket(self, string):
+    return re.sub(
+      r"(\d{2}|\d{1}),\s(\d{2}|\d{1}),\s(\d{2}|\d{1}),\s(\d{2}|\d{1}),\s(\d{2}|\d{1}),\s(\d{2}|\d{1})",
+      "",
+      string,
+    )
 
-    def find_ticket(self, text):
-        return re.compile(
-            r"(\d{2}|\d{1}),\s(\d{2}|\d{1}),\s(\d{2}|\d{1}),\s(\d{2}|\d{1}),\s(\d{2}|\d{1}),\s(\d{2}|\d{1})"
-        ).findall(text)
+  def otherWayToGenerateArray(self, n):
+    arr = list()
+    arr.append(str(datetime.datetime.today().isoformat())[0:10])
+    for i in range(n):
+      arr.append(self.generateNumber(1, 60, np.empty(n), i))
+    return arr
 
-    def remove_ticket(self, string):
-        return re.sub(
-            r"(\d{2}|\d{1}),\s(\d{2}|\d{1}),\s(\d{2}|\d{1}),\s(\d{2}|\d{1}),\s(\d{2}|\d{1}),\s(\d{2}|\d{1})",
-            "",
-            string,
-        )
+  def generateNumber(self, min=1, max=60, array=[], index=0):
+    rand = math.ceil(random.randint(min, max))
+    return self.generateNumber(min, max, array) if rand in array else rand
 
-    def otherWayToGenerateArray(self, n):
-        arr = list()
-        arr.append(str(datetime.datetime.today().isoformat())[0:10])
-        for i in range(n):
-            arr.append(self.generateNumber(1, 60, np.empty(n), i))
-        return arr
+  def create_payload_model_mega(self, qtd, filename="mega.csv"):
+    print("Start up...!")
+    accumated = list()
+    if not self.is_file_exists(filename):
+      accumated.append(self.otherWayToGenerateArray(6))
+      return accumated
 
-    def generateNumber(self, min=1, max=60, array=[], index=0):
-        rand = math.ceil(random.randint(min, max))
-        return self.generateNumber(min, max, array) if rand in array else rand
+    with open(self.path_database(filename), "rb") as readFile:
+      lines = len(readFile.readlines())
+      if lines > 1:
+        for i in range(qtd):
+          accumated.append(self.otherWayToGenerateArray(6))
+      else:
+        accumated.append(self.otherWayToGenerateArray(6))
+    readFile.close()
+    return accumated
 
-    def create_payload_model_mega(self, qtd, filename="mega.csv"):
-        print("Start up...!")
-        accumated = list()
-        if not self.is_file_exists(filename):
-            accumated.append(self.otherWayToGenerateArray(6))
-            return accumated
+  def is_file_exists(self, filename):
+    return os.path.exists(self.path_database(filename))
 
-        with open(self.path_database(filename), "rb") as readFile:
-            lines = len(readFile.readlines())
-            if lines > 1:
-                for i in range(qtd):
-                    accumated.append(self.otherWayToGenerateArray(6))
-            else:
-                accumated.append(self.otherWayToGenerateArray(6))
-        readFile.close()
-        return accumated
+  def getPeriod(self, period, lastDate, firstDate):
+    yearLastDate = int(lastDate.split("-")[0]) + period
+    yearFirstDate = int(firstDate.split("-")[0]) - period
 
-    def is_file_exists(self, filename):
-        return os.path.exists(self.path_database(filename))
+    dateMediaLast = (
+        str(yearLastDate)
+        + "-"
+        + str(lastDate.split("-")[1])
+        + "-"
+        + str(lastDate.split("-")[2])
+    )
 
-    def getPeriod(self, period, lastDate, firstDate):
-        yearLastDate = int(lastDate.split("-")[0]) + period
-        yearFirstDate = int(firstDate.split("-")[0]) - period
-
-        dateMediaLast = (
-            str(yearLastDate)
-            + "-"
-            + str(lastDate.split("-")[1])
-            + "-"
-            + str(lastDate.split("-")[2])
-        )
-
-        dateMediaFirst = (
-            str(yearFirstDate)
-            + "-"
-            + str(firstDate.split("-")[1])
-            + "-"
-            + str(firstDate.split("-")[2])
-        )
-        return {"dateMediaLast": dateMediaLast, "dateMediaFirst": dateMediaFirst}
+    dateMediaFirst = (
+        str(yearFirstDate)
+        + "-"
+        + str(firstDate.split("-")[1])
+        + "-"
+        + str(firstDate.split("-")[2])
+    )
+    return {"dateMediaLast": dateMediaLast, "dateMediaFirst": dateMediaFirst}
