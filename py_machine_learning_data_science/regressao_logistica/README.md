512ebda4e071159bc7246a3d9089fd63b131f588
Roberto Rosa <roberto.rosa7@gmail.com>
2023-02-24 16:01:21 -0300

    feat: logistic regression

---
+++ py_machine_learning_data_science/regressao_logistica/README.md
@@ -0,0 +1,9 @@
+# Regressão Logística
+
+| Métodos Preditivos        | Métodos Descritivos |
+| ------------------------- | ------------------- |
+| Classificação `(rótulos)` | Associação          |
+| Regressão `(números)`     | Agrupamentos        |
+|                           | Detecção de desvios |
+|                           | Padrões sequenciais |
+|                           | Sumarização         |
---
+++ py_mega_sena/model/model_mega.py
@@ -0,0 +1,22 @@
+class ModelMega:
+    concurso: int
+    data_do_sorteio: str
+    coluna1: int
+    coluna2: int
+    coluna3: int
+    coluna4: int
+    coluna5: int
+    coluna6: int
+    ganhadores_faixa_1: int or None
+    ganhadores_faixa_2: int or None
+    ganhadores_faixa_3: int or None
+    rateio_faixa_1: float or None
+    rateio_faixa_2: float or None
+    rateio_faixa_3: float or None
+    cidade: str or None
+    valor_arrecadado: float or None
+    estimativa_para_o_proximo_concurso: float or None
+    valor_acumulado_proximo_concurso: float or None
+    acumulado: float or None
+    sorteio_sspecial: str or None
+    observacao: str or None
